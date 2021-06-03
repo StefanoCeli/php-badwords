cb7ab623f20fc3d8210eea8aa06c4a5b68e0665b
StefanoCeli <stefanopsn91@gmail.com>
2021-06-03 17:23:43 +0200

    inizio esercizio

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>php-badwords</title>
+</head>
+<body>
+    
+<?php
+    $paragrafo="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nobis sit inventore labore. Tempora ut sequi atque nesciunt natus hic praesentium!";
+?>
+<h1><?php echo $paragrafo ?></h1>
+<h2>il paragrafo è lungo : <? echo strlen($paragrafo) ?> caratteri</h2>
+
+</body>
+</html>
